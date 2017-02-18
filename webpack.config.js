5ab1bf6b793202d0876ce7b0c9a89ec8c229d540
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-02-18 22:44:16 +0200

    added eslint verification

--- webpack.config.js
+++ webpack.config.js
@@ -4,40 +4,40 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './lib/index.js',
-    output: {
-        filename: 'client.js',
-        path: path.resolve(__dirname, 'bin')
-    },
-    externals: {
-        global: glob()
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components|browser)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            'engine.io-client': path.resolve(__dirname, 'browser/engine.io.js')
+  entry: './lib/index.js',
+  output: {
+    filename: 'client.js',
+    path: path.resolve(__dirname, 'bin')
+  },
+  externals: {
+    global: glob()
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components|browser)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
         }
-    },
-
-    plugins: [
-        new webpack.LoaderOptionsPlugin({minimize: true})//,
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false
-        //     }
-        // })
+      }
     ]
+  },
+  resolve: {
+    alias: {
+      'engine.io-client': path.resolve(__dirname, 'browser/engine.io.js')
+    }
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({minimize: true}) //,
+    // new webpack.optimize.UglifyJsPlugin({
+    //     compress: {
+    //         warnings: false
+    //     }
+    // })
+  ]
 }
 
 /**
@@ -45,9 +45,8 @@ module.exports = {
  *
  * @api private
  */
-
 function glob() {
-    return 'typeof self !== "undefined" ? self : ' +
-      'typeof window !== "undefined" ? window : ' +
-      'typeof global !== "undefined" ? global : {}';
+  return 'typeof self !== "undefined" ? self : ' +
+    'typeof window !== "undefined" ? window : ' +
+    'typeof global !== "undefined" ? global : {}';
 }
